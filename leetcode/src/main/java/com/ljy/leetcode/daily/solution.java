29ff136ce59e7b106f99ac79b106d12fa5cf975b
liucong <1149402522@qq.com>
2023-12-07 13:55:17 +0800

    init

--- leetcode/src/main/java/com/ljy/leetcode/daily/solution.java
+++ leetcode/src/main/java/com/ljy/leetcode/daily/solution.java
@@ -1,8 +1,78 @@
 package com.ljy.leetcode.daily;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author  ljy
  */
 public class solution {
+    private int ans;
+
+    /**
+     * 2023-12-05
+     * 2477. 到达首都的最少油耗
+     * @param roads
+     * @param seats
+     * @Da
+     * @return
+     */
+    public long minimumFuelCost(int[][] roads, int seats) {
+        int n = roads.length + 1;
+        List<Integer>[] g = new ArrayList[n];
+        Arrays.setAll(g, e -> new ArrayList<>());
+        for (int[] e : roads) {
+            int x = e[0], y = e[1];
+            g[x].add(y); // 记录每个点的邻居
+            g[y].add(x);
+        }
+
+        dfs(0, -1, g, seats);
+        return ans;
+    }
+
+    private int dfs(int x, int fa, List<Integer>[] g, int seats) {
+        int size = 1;
+        for (int y : g[x]) {
+            if (y != fa) { // 递归子节点，不能递归父节点
+                size += dfs(y, x, g, seats); // 统计子树大小
+            }
+        }
+        if (x > 0) { // x 不是根节点
+            ans += (size - 1) / seats + 1; // ceil(size/seats)
+        }
+        return size;
+    }
+
+    /**
+     * 2023/12/07
+     * 1466. 重新规划路线
+     * @param n
+     * @param connections
+     * @return
+     */
+    public int minReorder(int n, int[][] connections) {
+        List<int[]>[] e = new List[n];
+        for (int i = 0; i < n; i++) {
+            e[i] = new ArrayList<int[]>();
+        }
+        for (int[] edge : connections) {
+            e[edge[0]].add(new int[]{edge[1], 1});
+            e[edge[1]].add(new int[]{edge[0], 0});
+        }
+        return dfs(0, -1, e);
+    }
+
+    public int dfs(int x, int parent, List<int[]>[] e) {
+        int res = 0;
+        for (int[] edge : e[x]) {
+            if (edge[0] == parent) {
+                continue;
+            }
+            res += edge[1] + dfs(edge[0], x, e);
+        }
+        return res;
+    }
 
 }
